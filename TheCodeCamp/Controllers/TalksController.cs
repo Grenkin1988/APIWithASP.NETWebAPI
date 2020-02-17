da514ddcf098076fa81953dc4aa0a8e0732019d0
grenkin1988 <kruglovgleb@gmail.com>
2020-02-17 16:27:40 +0100

    Associations in APIs - part 1

---
+++ TheCodeCamp/Controllers/TalksController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+    [RoutePrefix("api/camps/{moniker}/talks")]
+    public class TalksController : BaseApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public TalksController(ICampRepository repository, IMapper mapper) {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [Route()]
+        public async Task<IHttpActionResult> Get(string moniker, bool includeSpeakers = false) {
+            async Task<IHttpActionResult> Function() {
+                var result = await _repository.GetTalksByMonikerAsync(moniker, includeSpeakers);
+                var mappedResult = _mapper.Map<TalkModel[]>(result);
+                return Ok(mappedResult);
+            }
+            return await SafeExecuteAsync(Function);
+        }
+
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> Get(string moniker, int id, bool includeSpeakers = false) {
+            async Task<IHttpActionResult> Function() {
+                var result = await _repository.GetTalkByMonikerAsync(moniker, id, includeSpeakers);
+                var mappedResult = _mapper.Map<TalkModel>(result);
+                return Ok(mappedResult);
+            }
+            return await SafeExecuteAsync(Function);
+        }
+    }
+}
